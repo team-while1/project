fcbf638acfb514d8b6c23eb0270eb2af44098882
KMW <sunnybikers@naver.com>
2025-01-21 16:37:10 +0900

    set up/KMW

---
+++ backend/src/test/java/com/school/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
